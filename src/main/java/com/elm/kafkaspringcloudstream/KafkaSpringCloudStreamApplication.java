478dfb334bbf14278785335e26423deb198e847a
AbderrahimeEl <elmoutaouakil12345@gmail.com>
2025-11-03 10:34:52 +0100

    initial commit

---
+++ src/main/java/com/elm/kafkaspringcloudstream/KafkaSpringCloudStreamApplication.java
@@ -0,0 +1,13 @@
+package com.elm.kafkaspringcloudstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringCloudStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaSpringCloudStreamApplication.class, args);
+    }
+
+}
